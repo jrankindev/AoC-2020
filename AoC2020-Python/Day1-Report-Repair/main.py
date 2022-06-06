c7e8c00811b94c1cb141ebf8468d8d9ee6e3ffad
JR <jxrankin@gmail.com>
2022-06-06 15:45:37 -0400

    formatting and functions

--- AoC2020-Python/Day1-Report-Repair/main.py
+++ AoC2020-Python/Day1-Report-Repair/main.py
@@ -1,16 +1,11 @@
-""" * ADVENT OF CODE 2020 - DAY 1 - REPORT REPAIR * """
+"""
+---   ADVENT OF CODE 2020
+---   DAY 1
+---   REPORT REPAIR
+"""
 
+import os
 
-# * ------------ PRE-PUZZLE CODE ------------ *
-
-# imports
-from os import system
-
-# clear screen for readability
-system("cls")
-
-# the input file we are using for this puzzle
-PUZZLE_FILE_INPUT = "pi.txt"
 
 # colors for print statements
 COLOR_HEADER = "\033[95m" + "\033[1m"
@@ -18,61 +13,84 @@ COLOR_QUESTION = "\033[94m"
 COLOR_ANSWER = "\033[92m"
 COLOR_CLEAR = "\033[0m"
 
-# write title and day
-print(f"{COLOR_HEADER} Advent of Code 2020 ")
-print(f" Day 1 - Report Repair \n{COLOR_CLEAR}")
 
-# grab puzzle input and store in list
-puzzle_input = []
-with open(PUZZLE_FILE_INPUT, encoding="UTF-8") as file:
-    for line in file:
-        line = int(line.strip())
-        puzzle_input.append(line)
-
-
-# * ------------ PUZZLE PART 1 ------------ *
-
-# loop through puzzle input finding entries that match 2020 - entry
-entries = []
-for element in puzzle_input:
-    searcher = 2020 - element
-    if searcher in puzzle_input:
-        entries.append(element)
-
-# calculate answer
-answer = entries[0] * entries[1]
-
-# write question and answer for part 1
-print(
-    f"{COLOR_QUESTION}Find the two entries that sum to 2020; what do you get if you"
-    + " multiply them together? ",
-    end="",
-)
-print(f"{COLOR_ANSWER} {str(answer)} \n {COLOR_CLEAR}")
+# puzzle input function
+def load_puzzle_input(puzzle_file_input: str):
+    puzzle_input = []
+    with open(puzzle_file_input, encoding="UTF-8") as file:
+        for line in file:
+            line = int(line.strip())
+            puzzle_input.append(line)
+    return puzzle_input
 
 
-# * ------------ PUZZLE PART 2 ------------ *
+# title printer
+def print_title(year: str, day: str, title: str):
+    print(f"{COLOR_HEADER} Advent of Code {year} ")
+    print(f" Day {day} - {title} \n{COLOR_CLEAR}")
 
-# loop through puzzle input twice on different numbers finding entries that match 2020
-# both entries
-entries.clear()
-for element in puzzle_input:
-    for element2 in puzzle_input:
-        if element != element2:
-            both_inputs = element + element2
-            searcher = 2020 - both_inputs
-            if searcher in puzzle_input and element2 in puzzle_input:
-                entries.append(element)
-                entries.append(element2)
-                entries.append(searcher)
 
-# calculate answer
-answer = entries[0] * entries[1] * entries[2]
+# print question and answer
+def print_question_answer(q: str, a: str):
+    print(f"{COLOR_QUESTION}{q}", end="",)
+    print(f"{COLOR_ANSWER} {str(a)} \n {COLOR_CLEAR}")
 
-# write question and answer for part 2
-print(
-    f"{COLOR_QUESTION}In your expense report, what is the product of the three"
-    + " entries that sum to 2020? ",
-    end="",
-)
-print(f"{COLOR_ANSWER} {str(answer)} \n {COLOR_CLEAR}")
+
+# puzzle part 1
+def puzzle_part_1(puzzle_input):
+    # loop through puzzle input finding entries that match 2020 - entry
+    entries = []
+    for element in puzzle_input:
+        searcher = 2020 - element
+        if searcher in puzzle_input:
+            entries.append(element)
+
+    # calculate answer
+    return entries[0] * entries[1]
+
+
+# puzzle part 2
+def puzzle_part_2(puzzle_input):
+    # loop through puzzle input twice on different numbers finding entries that
+    # match 2020 for both entries
+    entries = []
+    for element in puzzle_input:
+        for element2 in puzzle_input:
+            if element != element2:
+                both_inputs = element + element2
+                searcher = 2020 - both_inputs
+                if searcher in puzzle_input and element2 in puzzle_input:
+                    entries.append(element)
+                    entries.append(element2)
+                    entries.append(searcher)
+
+    # calculate answer
+    return entries[0] * entries[1] * entries[2]
+
+
+# main function
+def main():
+    # clear screen for readability (check to see if windows - nt)
+    os.system("cls" if os.name == "nt" else "clear")
+
+    # print title
+    print_title("2020", "1", "Report Repair")
+
+    # get puzzle input
+    puzzle_input = load_puzzle_input("pi.txt")
+
+    # solve part 1 and print QA
+    answer = puzzle_part_1(puzzle_input)
+    print_question_answer(
+        "Find the two entries that sum to 2020; what do you get if" +
+        " you multiply them together? ", answer)
+
+    # solve part 2 and print QA
+    answer = puzzle_part_2(puzzle_input)
+    print_question_answer(
+        "In your expense report, what is the product of the three entries" +
+        " that sum to 2020?", answer)
+
+
+if __name__ == '__main__':
+    main()
