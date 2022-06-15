57a9df65264a86a69e5e541fc743b57b48ac6dc6
JR <jxrankin@gmail.com>
2022-06-15 08:18:44 -0400

    removed namespace and added endl to couts

--- AoC2020-C++/Day01-Report-Repair/main.cpp
+++ AoC2020-C++/Day01-Report-Repair/main.cpp
@@ -5,37 +5,36 @@
 #include <fstream>
 #include <vector>
 #include <algorithm>
-using namespace std;
 
 int main()
 {
     // setup for title
-    const string TITLEYEAR = "2020";
-    const string TITLEDAY = "1";
-    const string TITLENAME = "Report Repair";
+    const std::string TITLEYEAR = "2020";
+    const std::string TITLEDAY = "1";
+    const std::string TITLENAME = "Report Repair";
 
     // setup for colors
-    const string COLOR_TITLE = "\033[1;95m";
-    const string COLOR_QUESTION = "\033[1;34m";
-    const string COLOR_ANSWER = "\033[1;32m";
-    const string COLOR_CLEAR = "\033[0m";
+    const std::string COLOR_TITLE = "\033[1;95m";
+    const std::string COLOR_QUESTION = "\033[1;34m";
+    const std::string COLOR_ANSWER = "\033[1;32m";
+    const std::string COLOR_CLEAR = "\033[0m";
 
     // puzzle file
-    const string PUZZLE_INPUT = "pi.txt";
+    const std::string PUZZLE_INPUT = "pi.txt";
 
     // clear screen for readability
     system("clear");
 
     // print title
-    cout << COLOR_TITLE << "\n     Advent of Code " << TITLEYEAR;
-    cout << "\n     Day " << TITLEDAY << " - " << TITLENAME << COLOR_CLEAR << "\n\n";
+    std::cout << COLOR_TITLE << "\n     Advent of Code " << TITLEYEAR << std::endl;
+    std::cout << "     Day " << TITLEDAY << " - " << TITLENAME << COLOR_CLEAR << std::endl;
 
     // open puzzle file and store lines in vector
-    vector<int> puzzleVector;
-    ifstream file(PUZZLE_INPUT);
+    std::vector<int> puzzleVector;
+    std::ifstream file(PUZZLE_INPUT);
     if (file.is_open())
     {
-        string line;
+        std::string line;
         while (getline(file, line))
             puzzleVector.push_back(stoi(line));
     }
@@ -43,7 +42,7 @@ int main()
 
     // * puzzle part 1
     // loop through puzzle input finding entries that match 2020 - entry
-    vector<int> entries;
+    std::vector<int> entries;
     for (int element : puzzleVector)
     {
         int searcher = 2020 - element;
@@ -55,9 +54,8 @@ int main()
     int answer = entries[0] * entries[1];
 
     // print question and answer
-    cout << COLOR_QUESTION << "Find the two entries that sum to 2020; what do you get if you multiply them together? " << COLOR_CLEAR;
-    cout << COLOR_ANSWER << answer << "\n\n"
-         << COLOR_CLEAR;
+    std::cout << COLOR_QUESTION << "\nFind the two entries that sum to 2020; what do you get if you multiply them together? " << COLOR_CLEAR;
+    std::cout << COLOR_ANSWER << answer << COLOR_CLEAR << std::endl;
 
     // * puzzle part 2
     // loop through puzzle input twice on different numbers finding entries that
@@ -85,9 +83,9 @@ int main()
     answer = entries[0] * entries[1] * entries[2];
 
     // print question and answer
-    cout << COLOR_QUESTION << "In your expense report, what is the product of the three entries that sum to 2020? " << COLOR_CLEAR;
-    cout << COLOR_ANSWER << answer << "\n\n"
-         << COLOR_CLEAR;
+    std::cout << COLOR_QUESTION << "\nIn your expense report, what is the product of the three entries that sum to 2020? " << COLOR_CLEAR;
+    std::cout << COLOR_ANSWER << answer << "\n"
+              << COLOR_CLEAR << std::endl;
 
     return 0;
 }
