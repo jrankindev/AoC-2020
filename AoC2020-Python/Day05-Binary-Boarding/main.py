af5e8f274cad5b8dce19ef1b2827acbfeb80eb93
JR <jxrankin@gmail.com>
2022-06-17 11:17:25 -0400

    format update

--- AoC2020-Python/Day05-Binary-Boarding/main.py
+++ AoC2020-Python/Day05-Binary-Boarding/main.py
@@ -1,63 +1,105 @@
-""" * ADVENT OF CODE 2020 - DAY 5 - BINARY BOARDING * """
+### ADVENT OF CODE 2020 DAY 5 BINARY BOARDING ###
 
 
-# * ------------ PRE-PUZZLE CODE ------------ *
+import os
 
-# imports
-from os import system
 
-# clear screen for readability
-system("cls")
-
-# the input file we are using for this puzzle
-PUZZLE_FILE_INPUT = "pi.txt"
-
-# colors for print statements
+# color constants for print statements
 COLOR_HEADER = "\033[95m" + "\033[1m"
 COLOR_QUESTION = "\033[94m"
 COLOR_ANSWER = "\033[92m"
 COLOR_CLEAR = "\033[0m"
 
-# write title and day
-print(f"{COLOR_HEADER} Advent of Code 2020 ")
-print(f" Day 5 - Binary Boarding \n{COLOR_CLEAR}")
 
-# grab puzzle input and store in list
-puzzle_input = []
-with open(PUZZLE_FILE_INPUT, encoding="UTF-8") as file:
-    for line in file:
-        line = line.strip()
-        puzzle_input.append(line)
-
-
-# * ------------ PUZZLE PART 1 ------------ *
-
-# replace characters with binary equivalent
-binary_puzzle_input = [
-    x.replace("F", "0").replace("B", "1").replace("L", "0").replace("R", "1")
-    for x in puzzle_input
-]
-
-# convert from string to base 2 (binary) int
-seats = [int(x, 2) for x in binary_puzzle_input]
-
-# sort to find largest seat id
-seats.sort()
-
-# write question and answer for part 1
-print(f"{COLOR_QUESTION}What is the highest seat ID on a boarding pass? ", end="")
-print(f"{COLOR_ANSWER} {str(seats[-1])} \n {COLOR_CLEAR}")
+## * input loader
+# return_type 1 is to load into list, otherwise load in variable
+# line_type 1 is to load each line as an string, otherwise load as int
+def load_puzzle_input(puzzle_file_input: str, return_type=1, line_type=1):
+    if return_type:
+        puzzle_input = []
+        with open(puzzle_file_input, encoding="UTF-8") as file:
+            for line in file:
+                if line_type:
+                    line = line.strip()
+                else:
+                    line = int(line.strip())
+                puzzle_input.append(line)
+        return puzzle_input
+    else:
+        with open(puzzle_file_input, encoding="UTF-8") as file:
+            puzzle_input = file.read()
+        return puzzle_input
 
 
-# * ------------ PUZZLE PART 2 ------------ *
+## * title printer
+def print_title(year: str, day: str, title: str):
+    print(f"{COLOR_HEADER} Advent of Code {year} ")
+    print(f" Day {day} - {title} \n{COLOR_CLEAR}")
 
-# find seat missing from list
-MY_SEAT = 0
-for i, seat_ID in enumerate(seats):
-    if seats[i + 1] - seats[i] != 1:
-        MY_SEAT = seats[i] + 1
-        break
 
-# write question and answer for part 2
-print(f"{COLOR_QUESTION}What is the ID of your seat? ", end="")
-print(f"{COLOR_ANSWER} {str(MY_SEAT)} \n {COLOR_CLEAR}")
+## * question and answer printer
+def print_question_answer(question: str, answer: str):
+    print(
+        f"{COLOR_QUESTION}{question}", end="",
+    )
+    print(f"{COLOR_ANSWER} {str(answer)} \n {COLOR_CLEAR}")
+
+
+## * puzzle part 1
+def puzzle_part_1(puzzle_input):
+    # replace characters with binary equivalent
+    binary_puzzle_input = [
+        x.replace("F", "0").replace("B", "1").replace("L", "0").replace("R", "1")
+        for x in puzzle_input
+    ]
+
+    # convert from string to base 2 (binary) int
+    seats = [int(x, 2) for x in binary_puzzle_input]
+
+    # sort to put largest seat id at end of list
+    seats.sort()
+
+    # return answer
+    return seats[-1], seats
+
+
+## * puzzle part 2
+def puzzle_part_2(seat_list):
+    # find seat missing from list in puzzle 1
+    my_seat = 0
+    seats = seat_list
+    for i in range(len(seats)):
+        if seats[i + 1] - seats[i] != 1:
+            my_seat = seats[i] + 1
+            break
+
+    # return answer
+    return my_seat
+
+
+## * main function
+def main():
+    # clear screen for readability (check to see if windows - nt)
+    os.system("cls" if os.name == "nt" else "clear")
+
+    # print title
+    print_title("2020", "5", "Binary Boarding")
+
+    # get puzzle input
+    puzzle_input = load_puzzle_input("pi.txt")
+
+    # solve part 1 and print QA
+    answer, seat_list = puzzle_part_1(puzzle_input)
+    print_question_answer(
+        "What is the highest seat ID on a boarding pass? ", answer,
+    )
+
+    # solve part 2 and print QA
+    answer = puzzle_part_2(seat_list)
+    print_question_answer(
+        "What is the ID of your seat? ", answer,
+    )
+
+
+if __name__ == "__main__":
+    main()
