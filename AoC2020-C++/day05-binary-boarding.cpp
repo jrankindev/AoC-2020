d727ab676e82449760fd77d38fd2534b41f2b235
JR <jxrankin@gmail.com>
2022-07-01 09:51:16 -0400

    day5 complete

--- AoC2020-C++/day05-binary-boarding.cpp
+++ AoC2020-C++/day05-binary-boarding.cpp
@@ -1,18 +1,49 @@
 // * ADVENT OF CODE 2020 - DAY 5 - BINARY BOARDING *
 
+#include <algorithm>
+
 #include "helper/helper.h"
 
-// * puzzle part 1 function
-int puzzlePart1(std::vector<std::string> puzzleVector) {
-    for (auto line : puzzleVector) {
-        std::cout << line << std::endl;
+// * puzzle part 2 function
+int puzzlePart2(std::vector<int> puzzleVector) {
+    // seat tracking var
+    int mySeat = 0;
+
+    // loop through each element in vector
+    for (unsigned long int i = 0; i < puzzleVector.size(); i++) {
+        // check to see if the next element minus current element does not equal 1
+        // if so, we have a missing value which should be our seat
+        if ((puzzleVector[i + 1] - puzzleVector[i]) != 1) {
+            mySeat = puzzleVector[i] + 1;
+            break;
+        }
     }
-    return 0;
+
+    // return answer
+    return mySeat;
 }
 
-// * puzzle part 2 function
-int puzzlePart2(std::vector<std::string> puzzleVector) {
-    return 0;
+// * binary replace characters, convert to base 10, sort
+std::vector<int> vecBinaryReplaceConvertSort(std::vector<std::string> puzzleVector) {
+    // replace characters with binary equivalent
+    for (unsigned long int i = 0; i < puzzleVector.size(); i++) {
+        std::replace(puzzleVector[i].begin(), puzzleVector[i].end(), 'F', '0');
+        std::replace(puzzleVector[i].begin(), puzzleVector[i].end(), 'B', '1');
+        std::replace(puzzleVector[i].begin(), puzzleVector[i].end(), 'L', '0');
+        std::replace(puzzleVector[i].begin(), puzzleVector[i].end(), 'R', '1');
+    }
+
+    // convert from string to base 2 (binary) int
+    std::vector<int> puzzleVectorInt;
+    for (auto line : puzzleVector) {
+        puzzleVectorInt.push_back(std::stoi(line, nullptr, 2));
+    }
+
+    // sort to put largest seat id at end of list
+    sort(puzzleVectorInt.begin(), puzzleVectorInt.end());
+
+    // return new converted vector
+    return puzzleVectorInt;
 }
 
 // * main function
@@ -21,19 +52,22 @@ int main() {
     helper::printTitle("2020", "5", "Binary Boarding");
 
     // load puzzle input
-    std::vector<std::string> puzzleVector = helper::loadPuzzleInputStringVec("inputs/day05/pie.txt");
+    std::vector<std::string> puzzleVector = helper::loadPuzzleInputStringVec("inputs/day05/pi.txt");
+
+    // setup converted base 10 vector
+    std::vector<int> puzzleVectorInt = vecBinaryReplaceConvertSort(puzzleVector);
 
     // solve part 1
-    int answer = puzzlePart1(puzzleVector);
+    int answer = puzzleVectorInt.back();
 
     // print question and answer
-    helper::printQuestionAnswer("\nIn your batch file, how many passports are valid? ", std::to_string(answer));
+    helper::printQuestionAnswer("\nWhat is the highest seat ID on a boarding pass? ", std::to_string(answer));
 
     // solve part 2
-    answer = puzzlePart2(puzzleVector);
+    answer = puzzlePart2(puzzleVectorInt);
 
     // print question and answer
-    helper::printQuestionAnswer("In your batch file, how many passports are valid? ", std::to_string(answer));
+    helper::printQuestionAnswer("What is the ID of your seat? ", std::to_string(answer));
 
     return 0;
 }
