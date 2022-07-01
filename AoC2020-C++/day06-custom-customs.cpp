d24e2116984537764f321d0d8bb2b8b81bf60c0f
JR <jxrankin@gmail.com>
2022-07-01 12:03:00 -0400

    day6 completed

--- AoC2020-C++/day06-custom-customs.cpp
+++ AoC2020-C++/day06-custom-customs.cpp
@@ -1,20 +1,76 @@
 // * ADVENT OF CODE 2020 - DAY 6 - CUSTOM CUSTOMS *
 
+#include <bits/stdc++.h>
+
 #include <regex>
 
 #include "helper/helper.h"
 
 // * puzzle part 1 function
 int puzzlePart1(std::vector<std::string> puzzleVector) {
+    // declare unordered set to track questions for each group
+    std::unordered_set<char> questionSet;
+
+    // variable to track the total answered questions for all groups
+    int totalAnswers = 0;
+
+    // loop through each group
     for (auto line : puzzleVector) {
-        std::cout << line << std::endl;
+        // loop through each answer in the gruop
+        for (auto character : line) {
+            // if not a blank character, then insert character into set
+            if (character != ' ') {
+                questionSet.insert(character);
+            }
+        }
+
+        // add the size of the set to total answers and then clear the set for next group
+        totalAnswers += questionSet.size();
+        questionSet.clear();
     }
-    return 0;
+
+    // return total questions answered
+    return totalAnswers;
 }
 
 // * puzzle part 2 function
 int puzzlePart2(std::vector<std::string> puzzleVector) {
-    return 0;
+    // declare unordered set to track questions for each group
+    std::unordered_set<char> questionSet;
+
+    // variable to track the total answered questions for the group
+    int totalAnswers = 0;
+
+    // loop through each group
+    for (auto line : puzzleVector) {
+        // total number of people in the group
+        size_t groupCount = std::count(line.begin(), line.end(), ' ') + 1;
+
+        // loop through each answer in the group
+        for (auto character : line) {
+            // if not a blank character, then insert character into set
+            if (character != ' ') {
+                questionSet.insert(character);
+            }
+        }
+
+        // loop through each answered question in the set
+        for (auto character : questionSet) {
+            // total number of each question answered in the group
+            size_t characterOccurences = std::count(line.begin(), line.end(), character);
+
+            // if all members of the group answered yes then add one to total answers
+            if (groupCount == characterOccurences) {
+                totalAnswers++;
+            }
+        }
+
+        // clear the set for the next group
+        questionSet.clear();
+    }
+
+    // return total
+    return totalAnswers;
 }
 
 // * main function
@@ -23,7 +79,7 @@ int main() {
     helper::printTitle("2020", "6", "Custom Customs");
 
     // load puzzle input
-    std::string puzzleString = helper::loadPuzzleInputString("inputs/day06/pie.txt");
+    std::string puzzleString = helper::loadPuzzleInputString("inputs/day06/pi.txt");
 
     // remove all blank lines and put passport fields all on a single line
     puzzleString = std::regex_replace(puzzleString, std::regex("\r\n"), " ");
